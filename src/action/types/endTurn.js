661e5d5c3298b359bdaabaa6865a79f22b9396ec
neynn <nils.lucky@web.de>
2026-01-05 17:59:52 +0100

    Update endTurn.js

--- src/action/types/endTurn.js
+++ src/action/types/endTurn.js
@@ -7,21 +7,21 @@ export const EndTurnAction = function() {
 EndTurnAction.prototype = Object.create(Action.prototype);
 EndTurnAction.prototype.constructor = EndTurnAction;
 
-EndTurnAction.prototype.onStart = function(gameContext, data) {
+EndTurnAction.prototype.isFinished = function(gameContext, executionPlan) {
+    return true;
+}
+
+EndTurnAction.prototype.onEnd = function(gameContext, data) {
+    this.execute(gameContext, data);
+}
+
+EndTurnAction.prototype.execute = function(gameContext, data) {
     const { world } = gameContext;
     const { turnManager } = world;
     const { actorID } = data;
 
     turnManager.cancelActorActions();
 }
-
-EndTurnAction.prototype.onUpdate = function(gameContext, data) {}
-
-EndTurnAction.prototype.isFinished = function(gameContext, executionPlan) {
-    return true;
-}
-
-EndTurnAction.prototype.onEnd = function(gameContext, data) {}
 
 EndTurnAction.prototype.fillExecutionPlan = function(gameContext, executionPlan, actionIntent) {
     const { world } = gameContext;
