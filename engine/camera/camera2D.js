43348eab2b28795bdbce52fdf7b20312273986d5
neynn <nils.lucky@web.de>
2026-03-10 04:00:03 +0100

    Update camera2D.js

--- engine/camera/camera2D.js
+++ engine/camera/camera2D.js
@@ -1,11 +1,11 @@
 import { DEBUG } from "../debug.js";
 import { clampValue } from "../math/math.js";
 import { TextureHandle } from "../resources/texture/textureHandle.js";
-import { Camera } from "./camera.js";
 
 export const Camera2D = function() {
-    Camera.call(this);
-
+    //These values record the current tile position of the camera2D.
+    //offsetX and offsetY are the offsets of the tile position.
+    //startX, startY, endX, endY are the positions INSIDE the buffer.
     this.tileX = 0;
     this.tileY = 0;
     this.offsetX = 0;
@@ -18,9 +18,42 @@ export const Camera2D = function() {
     this.mapHeight = 0;
     this.tileWidth = 1;
     this.tileHeight = 1;
-    this.halfTileWidth = 1;
-    this.halfTileHeight = 1;
+    this.halfTileWidth = 0.5;
+    this.halfTileHeight = 0.5;
+    this.scale = 1;
+
+    this.fViewportX = 0;
+    this.fViewportY = 0;
+    this.viewportX = 0;
+    this.viewportY = 0;
+    this.viewportX_limit = 0;
+    this.viewportY_limit = 0;
+
+    this.viewportWidth = 0;
+    this.viewportHeight = 0;
+    this.wViewportWidth = 0; //WorldViewportWidth
+    this.wViewportHeight = 0; //WorldViewportHeight
+    this.sViewportWidth = 0; //ScreenViewportWidth
+    this.sViewportHeight = 0; //ScreenViewportHeight
+    this.worldWidth = 0;
+    this.worldHeight = 0;
+    this.viewportMode = Camera2D.VIEWPORT_MODE.DRAG;
+    this.viewportType = Camera2D.VIEWPORT_TYPE.BOUND;
+    this.currentFrame = 0;
+
+    //worldWidth and worldHeight are no longer needed, as are NOT viewportX, viewportY
 }
+
+Camera2D.VIEWPORT_TYPE = {
+    FREE: 0,
+    BOUND: 1
+};
+
+Camera2D.VIEWPORT_MODE = {
+    FIXED: 0,
+    FOLLOW: 1,
+    DRAG: 2
+};
 
 Camera2D.COLOR = {
     EMPTY_TILE_FIRST: "#000000",
@@ -32,8 +65,111 @@ Camera2D.MAP_OUTLINE = {
     COLOR: "#dddddd"
 };
 
-Camera2D.prototype = Object.create(Camera.prototype);
-Camera2D.prototype.constructor = Camera2D;
+Camera2D.prototype.update = function(gameContext, renderContext) {}
+
+Camera2D.prototype.applyBounds = function() {
+    if(this.viewportType === Camera2D.VIEWPORT_TYPE.BOUND) {
+        if(this.viewportX < 0) {
+            this.viewportX = 0;
+        } else if(this.viewportX >= this.viewportX_limit) {
+            this.viewportX = this.viewportX_limit;
+        }
+    
+        if(this.viewportY < 0) {
+            this.viewportY = 0;
+        } else if(this.viewportY >= this.viewportY_limit) {
+            this.viewportY = this.viewportY_limit;
+        }
+    }
+
+    this.fViewportX = Math.floor(this.viewportX);
+    this.fViewportY = Math.floor(this.viewportY);
+}
+
+Camera2D.prototype.bindViewport = function() {
+    this.viewportType = Camera2D.VIEWPORT_TYPE.BOUND;
+    this.applyBounds();
+}
+
+Camera2D.prototype.freeViewport = function() {
+    this.viewportType = Camera2D.VIEWPORT_TYPE.FREE;
+}
+
+Camera2D.prototype.reloadViewport = function() {
+    this.wViewportWidth = this.viewportWidth / this.scale;
+    this.wViewportHeight = this.viewportHeight / this.scale;
+    this.sViewportWidth = this.viewportWidth * this.scale;
+    this.sViewportHeight = this.viewportHeight * this.scale;
+
+    if(this.worldWidth <= this.wViewportWidth) {
+        this.viewportX_limit = 0;
+    } else {
+        this.viewportX_limit = this.worldWidth - this.wViewportWidth;
+    }
+
+    if(this.worldHeight <= this.wViewportHeight) {
+        this.viewportY_limit = 0;
+    } else {
+        this.viewportY_limit = this.worldHeight - this.wViewportHeight;
+    }
+
+    this.applyBounds();
+}
+
+Camera2D.prototype.setWorldSize = function(worldWidth, worldHeight) {
+    this.worldWidth = worldWidth;
+    this.worldHeight = worldHeight;
+    this.reloadViewport();
+}
+
+Camera2D.prototype.setScale = function(scale) {
+    const MIN_SCALE = 1;
+
+    if(scale < MIN_SCALE) {
+        this.scale = MIN_SCALE;
+    } else {
+        this.scale = scale;
+    }
+
+    this.reloadViewport();
+}
+
+Camera2D.prototype.setViewportSize = function(width, height) {
+    this.viewportWidth = width;
+    this.viewportHeight = height;
+    this.reloadViewport();
+}
+
+Camera2D.prototype.moveViewport = function(viewportX, viewportY) {
+    if(this.viewportMode !== Camera2D.VIEWPORT_MODE.FIXED) {
+        this.viewportX = viewportX;
+        this.viewportY = viewportY;
+        this.applyBounds();
+    }
+}
+
+Camera2D.prototype.dragViewport = function(deltaX, deltaY) {
+    if(this.viewportMode === Camera2D.VIEWPORT_MODE.DRAG) {
+        const viewportX = this.viewportX + deltaX / this.scale;
+        const viewportY = this.viewportY + deltaY / this.scale;
+        
+        this.moveViewport(viewportX, viewportY);
+    }
+}
+
+Camera2D.prototype.centerViewportOn = function(positionX, positionY) {
+    const viewportX = positionX - this.viewportWidth / 2;
+    const viewportY = positionY - this.viewportHeight / 2;
+
+    this.moveViewport(viewportX, viewportY);
+}
+
+Camera2D.prototype.centerViewportOnWorld = function() {
+    const viewportX = this.worldWidth / 2;
+    const viewportY = this.worldHeight / 2;
+
+    this.centerViewportOn(viewportX, viewportY);
+}
 
 Camera2D.prototype.onMapSizeUpdate = function(oldWidth, oldHeight) {}
 
